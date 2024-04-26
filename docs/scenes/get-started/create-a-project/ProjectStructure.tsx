8d60d968b585e47e5d1642e25fc8d0dc92064732
Jon Samp <sampjon@gmail.com>
2024-04-25 22:49:25 -0500

    adds scenes

---
+++ docs/scenes/get-started/create-a-project/ProjectStructure.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {
+  // props
+};
+
+export function ProjectStructure(props: Props) {
+  return <div>ProjectStructure</div>;
+}
---
+++ docs/scenes/get-started/create-a-project/TemplateFeatures.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {
+  // props
+};
+
+export function TemplateFeatures(props: Props) {
+  return <div>TemplateFeatures</div>;
+}
